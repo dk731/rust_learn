02ff3aa59740d514841538b77032b809cb3cdd5c
dk731 <demid.kaidalov@gmail.com>
2022-09-13 13:05:47 +0300

    Finsihed one threaded solution

--- webserver/src/HTTP/mod.rs
+++ webserver/src/HTTP/mod.rs
@@ -1,11 +1,13 @@
 use serde::{Deserialize, Serialize};
-use std::str;
+use std::{collections::HashMap, hash::Hash, os::linux::raw, str};
 
-enum Methode {
+#[derive(Debug)]
+pub enum Methode {
     GET,
     POST,
     DELETE,
     //...
+    INVALID,
 }
 
 struct Body {
@@ -13,24 +15,49 @@ struct Body {
     parsed: Option<i32>,
 }
 
-pub struct Response {}
+pub struct Response<'a> {
+    status_code: u32,
+    reason: String,
+    pub headers: HashMap<String, String>,
+    body: Option<&'a str>,
+}
 
 pub struct Request<'a> {
     methode: Methode,
+    protocol: &'a str,
     request_path: &'a str,
-    body: Option<String>,
-    raw: String,
+    body: Option<Body>,
+    raw: &'a str,
+}
+
+impl Methode {
+    pub fn new(name: &str) -> Self {
+        match name {
+            "GET" => Methode::GET,
+            "POST" => Methode::POST,
+            "DELETE" => Methode::DELETE,
+            _ => Methode::INVALID,
+        }
+    }
 }
 
 impl<'a> Request<'a> {
-    pub fn new(req_buf: [u8; 1024]) -> Option<Self> {
-        let raw_req = str::from_utf8(&req_buf).unwrap().to_string();
+    pub fn new(req_buf: &'a [u8; 1024]) -> Result<Self, String> {
+        let raw = str::from_utf8(req_buf).unwrap();
+        let mut lines = raw.lines();
 
-        Some(Request {
-            methode: Methode::GET,
-            request_path: &raw_req[1..10],
+        let mut req_line = lines.next().unwrap().split(" ");
+
+        let methode = Methode::new(req_line.next().unwrap());
+        let request_path = req_line.next().unwrap();
+        let protocol = req_line.next().unwrap();
+
+        Ok(Request {
+            methode,
+            protocol,
+            request_path,
             body: None,
-            raw: raw_req.to_string(),
+            raw,
         })
     }
 
@@ -38,7 +65,44 @@ impl<'a> Request<'a> {
         &self.methode
     }
 
-    pub fn path(&self) -> &str {
+    pub fn protocol(&self) -> &str {
+        self.protocol
+    }
+
+    pub fn request_path(&self) -> &str {
         self.request_path
+    }
+}
+
+impl<'a> Response<'a> {
+    pub fn new() -> Self {
+        Response {
+            status_code: 200,
+            reason: "OK".to_string(),
+            headers: HashMap::new(),
+            body: None,
+        }
+    }
+
+    pub fn set_body(&mut self, new_body: &'a str) {
+        self.headers
+            .insert("Content-Length".to_string(), new_body.len().to_string());
+        self.body = Some(new_body);
+    }
+
+    pub fn result(&self) -> String {
+        let mut res = format!("HTTP/1.1 {} {}\r\n", self.status_code, self.reason);
+
+        for (k, v) in &self.headers {
+            res.push_str(&format!("{}: {}\r\n", k, v));
+        }
+        res.push_str("\r\n");
+
+        if let Some(body) = self.body {
+            res.push_str(body);
+            res.push_str("\r\n");
+        }
+
+        res
     }
 }
--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,4 @@
+use chrono;
 use std::{
     fs,
     io::{BufRead, BufReader, Read, Write},
@@ -21,7 +22,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut input_buffer = [0; 1024];
     stream.read(&mut input_buffer).unwrap();
 
-    let req = HTTP::Request::new(input_buffer).unwrap();
+    let req = HTTP::Request::new(&input_buffer).unwrap();
+    let mut res = HTTP::Response::new();
 
+    res.headers
+        .insert("Date".to_string(), chrono::offset::Utc::now().to_string());
+
+    res.set_body(&html_str);
+
+    stream.write_all(res.result().as_bytes());
     // println!("{response}");
 }
